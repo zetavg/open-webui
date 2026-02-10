4c0804abac1708cfb5d389562f7d8c104ddaf693
zetavg <mail@zeta.vg>
2026-02-10 02:52:48 +0100

    [patch] redirect model profile image URLs starting with /

--- backend/open_webui/routers/models.py
+++ backend/open_webui/routers/models.py
@@ -352,7 +352,9 @@ def get_model_profile_image(
         etag = f'"{model.updated_at}"' if model.updated_at else None
 
         if model.meta.profile_image_url:
-            if model.meta.profile_image_url.startswith("http"):
+            if model.meta.profile_image_url.startswith(
+                ("http", "/")
+            ):
                 return Response(
                     status_code=status.HTTP_302_FOUND,
                     headers={"Location": model.meta.profile_image_url},
@@ -375,7 +377,6 @@ def get_model_profile_image(
                     )
                 except Exception as e:
                     pass
-
         return FileResponse(f"{STATIC_DIR}/favicon.png")
     else:
         return FileResponse(f"{STATIC_DIR}/favicon.png")
